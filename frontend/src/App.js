e01c25dfa1453527ff459b5305515dd6eef211b5
Shrestha Datta <52985769+Shrestha30@users.noreply.github.com>
2023-09-20 12:32:12 +0600

    create baic framework of react, routers, react-toastify

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginRegister from "./pages/LoginRegister";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <LoginRegister/>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
