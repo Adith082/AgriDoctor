2dd95bd048c154f1590dbb71902a8628dff757aa
Adith082 <adibhasan35@gmail.com>
2023-09-26 14:08:59 +0800

    crop services implemented

---
+++ Backend/agridoctor-app-apis/src/main/java/com/visionaryproviders/agridoctor/payloads/CropRecommendationRequestDto.java
@@ -0,0 +1,12 @@
+package com.visionaryproviders.agridoctor.payloads;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CropRecommendationRequestDto {
+	
+	private double nitrogen, phosphorous, pottasium, ph, rainfall, temperature, humidity;
+	private String city;
+}
